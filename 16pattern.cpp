b2c80a35f550335a89663cecd4d50f69866fd5dc
naveen <chippalanaveen1@gmail.com>
2024-01-20 23:16:18 +0530

    16th pattern file

---
+++ 16pattern.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int k;
+for(int i=1;i<=5;i++)
+{
+
+    for(int j=0;j<=4;j++)
+    {
+        k=j+i;
+        cout<<" "<<k;
+    }
+ cout<<endl;
+}
+}
