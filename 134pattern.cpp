249c5aa0857222380c638dd325fa738a33134c21
naveen <chippalanaveen1@gmail.com>
2024-01-29 23:53:04 +0530

    134 pattern file added
    simply done

---
+++ 134pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,k=0;
+for(i=5;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        cout<<" "<<i;
+    }
+        k++;
+        cout<<endl;
+        for(int j=1;j<=k;j++){
+
+    cout<<" ";
+        }
+}
+return 0;
+}
+
